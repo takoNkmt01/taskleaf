53953e56cf099a195d72cb6cc9362f634eacbca8
takoNkmt01 <0cto.pass0512@gmail.com>
2020-02-02 18:23:11 +0900

    prepared task_mailer

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -1,2 +1,15 @@
 class TaskMailer < ApplicationMailer
+   #メールの送信元を統一する
+   default from: 'taskleaf@example.com'
+
+   def creation_email(task)
+      #引数で登録したTaskオブジェクトを後でメール本文のテンプレートで流用するためインスタンス変数に代入する。
+      @task = task
+      #メール作成、送信を行うmailメソッド。from、to、subjectなどを指定する。
+      mail(
+         subject: 'タスク作成完了メール'
+         to: 'user@example.com'
+         from: 'taskleaf@example.com'
+      )
+   end
 end
