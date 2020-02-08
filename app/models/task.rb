641c7e63ed5e2ccd95a06d1b169b724f87e008a2
takoNkmt01 <0cto.pass0512@gmail.com>
2020-02-08 18:41:36 +0900

    add csv import and export option

--- app/models/task.rb
+++ app/models/task.rb
@@ -15,6 +15,34 @@ class Task < ApplicationRecord
       %w[name created_at]
    end
 
+   #属性と出力する順番を定義する
+   def self.csv_attributes
+      ["name", "description", "created_at", "updated_at"]
+   end
+
+   #csv出力
+   def self.generate_csv
+      #CSVデータの文字列生成
+      CSV.generate(headers: true) do |csv|
+         #一行目のヘッダを出力する
+         csv << csv_attributes
+         #全タスクを取得し一行ごとに出力する
+         all.each do |task|
+            csv << csv_attributes.map{ |attr| task.send(attr) }
+         end
+      end
+   end
+
+   #CSVインポート
+   def self.import(file)
+      CSV.foreach(file.path, headers: true) do |row|
+         task = new
+         #to_hashメソッドで値をヘッダーに対応したHash形式で取得する。
+         task.attributes = row.to_hash.slice(*csv_attributes)
+         task.save!
+      end
+   end
+
    #ransackable_associationsで検索条件に含める関連を指定する。空の配列を返すように指定すれば意図しない関連を排除できる。
    def self.ransackable_associations(auth_object = nill)
       []
--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,7 @@
 require_relative 'boot'
 
+#csvファイルをアプリで扱うためにrequireする
+require 'csv'
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
